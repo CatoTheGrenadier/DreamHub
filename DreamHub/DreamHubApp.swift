3cd53ba9be7a4d02c97801dc2fbd6da4beab69e6
Cato the Chevalier <131333594+CatoTheGrenadier@users.noreply.github.com>
2025-06-26 21:11:14 -0400

    Initial Commit

---
+++ DreamHub/DreamHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamHubApp.swift
+//  DreamHub
+//
+//  Created by Yi Ling on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct DreamHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
